f48c9c1c7bbba852980691d2abb11c914c371bf9
Mustafa Ghouri <112586426+dev4you2k16@users.noreply.github.com>
2025-11-14 01:39:43 +0500

    Hero Section responsive

--- resources/views/front/home/_partials/hero-section.blade.php
+++ resources/views/front/home/_partials/hero-section.blade.php
@@ -1,5 +1,5 @@
-<section class="container grid grid-cols-2 gap-12">
-    <div class="mt-28">
+<section class="container grid lg:grid-cols-2 grid-cols-1 gap-12">
+    <div class="flex flex-col justify-center lg:items-start items-center lg:text-start text-center">
         <h1 class="mb-8 text-secondary">
             Schedule Powerful Content, Reach More People and Save Hours Every Week.
         </h1>
@@ -8,13 +8,15 @@
 
         <p class="font-medium mb-4">No credit card required.</p>
 
-        <button class="button-primary">
-            <h5>Start Free Account</h5>
-            <img src="/assets/images/svg-icons/arrow-up-right.svg" alt="" />
-        </button>
+        <div>
+            <button class="button-primary">
+                <h5>Start Free Account</h5>
+                <img src="/assets/images/svg-icons/arrow-up-right.svg" alt="" />
+            </button>
+        </div>
     </div>
 
-    <div class="flex justify-end">
-        <img src="/assets/images/home-page/hero-section.png" alt="" class="h-[80vh] rounded-[4.5rem]" />
+    <div class="flex lg:justify-end justify-center">
+        <img src="/assets/images/home-page/hero-section.png" alt="" class="xl:h-[80vh] h-[70vh] rounded-[4.5rem]" />
     </div>
 </section>
--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -5,17 +5,17 @@
     @include('front.home._partials.hero-section')
 
     {{-- Key Points --}}
-    @include('front.home._partials.key-points')
+    {{-- @include('front.home._partials.key-points') --}}
 
     {{-- High Lights --}}
-    @include('front.home._partials.high-lights')
+    {{-- @include('front.home._partials.high-lights') --}}
 
     {{-- Pricing Plan --}}
-    @include('front.home._partials.pricing-plan')
+    {{-- @include('front.home._partials.pricing-plan') --}}
 
     {{-- Section 6 --}}
-    @include('front.home._partials.section-6')
+    {{-- @include('front.home._partials.section-6') --}}
 
     {{-- Why Choose Us --}}
-    @include('front.home._partials.why-choose-us')
+    {{-- @include('front.home._partials.why-choose-us') --}}
 @endsection
